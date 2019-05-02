f1aeb661e95bf7be46995f2eb6aea2a4b098e1b6
Fran Yoga <ing.francisco.oliva@gmail.com>
2019-05-02 10:56:44 -0300

    cambiado el Main

---
+++ src/main/java/MagnetoApi.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * @author foliva
+ */
+import static spark.Spark.*;
+public class MagnetoApi {
+
+    public MagnetoApi(){
+        port(getHerokuAssignedPort());
+        System.out.println("Creando API Magneto");
+        get("/hello", (req, res) -> "Hello World");
+
+    }
+
+
+    public static void main(String[] args) {
+        MagnetoApi magnetoApi = new MagnetoApi();
+    }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+}
